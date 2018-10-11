65051b5c5dd4f35ac2efd5bac4b4d34c93f6bfe2
Sam Schweigel <s.schweigel@gmail.com>
2018-10-11 14:43:46 -0700

    Re-structured driver commands, will fix later

--- src/STLink/Driver.hs
+++ src/STLink/Driver.hs
@@ -1,9 +1,14 @@
+{-# LANGUAGE FlexibleContexts #-}
+
 module STLink.Driver
-  ( openBoard
+  ( 
   ) where
 
 import Control.Monad
+import Control.Monad.Except
+
 import System.USB
+
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Lazy as BL
 
@@ -12,6 +17,69 @@ import Data.Binary.Get
 import Data.Bits
 
 import STLink.Detection
+
+bitsOn :: (Num a, Bits a) => Int -> a
+bitsOn n = f n 0
+  where
+    f 0 m = shiftR m 1
+    f n m = f (n-1) (shift (m .|. 1) 1)
+
+selectBits :: (Integral a, Bits a, Integral n) => (Int, Int) -> a -> n
+selectBits (p, w) n = fromIntegral $ shiftR n p .&. bitsOn w
+
+eguard :: Monad m => e -> Bool -> ExceptT e m ()
+eguard e True = throwError e
+eguard e False = pure ()
+
+data OutCommand c d =
+  OutCommand c d
+  deriving (Show, Eq)
+
+data InCommand c r =
+  InCommand c Size
+  deriving (Show, Eq)
+
+decodeResp :: (MonadError String m, Binary r) => BS.ByteString -> m r
+decodeResp d = either f s . decodeOrFail . BL.fromStrict $ d
+  where
+    f (_, _, m) = throwError $ "failed to decode response: " ++ m
+    s (_, _, r) = pure r
+
+inCommand :: (Binary c, Binary r) => DeviceHandle -> InCommand c r -> ExceptT String IO r
+inCommand h (InCommand c s) = do
+  -- send the command and make sure it was sucessful
+  let cd = BL.toStrict . encode $ c
+  (size, sendStatus) <- lift $ writeBulk h (EndpointAddress 1 Out) cd 1000
+  eguard "sent command truncated" $ size /= BS.length cd
+  eguard "send timed out" $ sendStatus /= Completed
+
+  -- receive the response, and ensure it's what we expected
+  (d, recStatus) <- lift $ readBulk h (EndpointAddress 1 In) s 1000
+  eguard "response truncated" $ s /= BS.length d
+  eguard "receive timed out" $ recStatus /= Completed
+  decodeResp d
+
+outCommand :: (Binary c, Binary d) => DeviceHandle -> OutCommand c d -> ExceptT String IO ()
+outCommand h (OutCommand c d) = do
+  -- send the command and make sure it was sucessful
+  let cd = BL.toStrict . encode $ c
+  (sendSize, sendStatus) <- lift $ writeBulk h (EndpointAddress 1 Out) cd 1000
+  eguard "sent command truncated" $ sendSize /= BS.length cd
+  eguard "send command timed out" $ sendStatus /= Completed
+
+  -- send the data
+  let dd = BL.toStrict . encode $ d
+  (recSize, recStatus) <- lift $ writeBulk h (EndpointAddress 1 Out) dd 1000
+  eguard "sent data truncated" $ recSize /= BS.length dd
+  eguard "send data timed out" $ recStatus /= Completed
+
+data CommandVersion
+  = CommandVersion
+  deriving (Show, Eq)
+
+instance Binary CommandVersion where
+  put _ = putWord8 0xf1
+  get = undefined
 
 data ResponseVersion
   = ResponseVersion
@@ -30,47 +98,14 @@ instance Binary ResponseVersion where
       (selectBits (6, 6) n)
       (selectBits (0, 6) n)
 
-bitsOn :: (Num a, Bits a) => Int -> a
-bitsOn n = f n 0
-  where
-    f 0 m = shiftR m 1
-    f n m = f (n-1) (shift (m .|. 1) 1)
+type VersionCommand = InCommand CommandVersion ResponseVersion
 
-selectBits :: (Integral a, Bits a, Integral n) => (Int, Int) -> a -> n
-selectBits (p, w) n = fromIntegral $ shiftR n p .&. bitsOn w
+-- withBoard :: ExceptT String IO a
+-- withBoard = pickBoard >>=
+  -- maybe (pure ()) claimBoard
 
-getVersion :: DeviceHandle -> IO ResponseVersion
-getVersion h =
-  decode . BL.fromStrict <$> inCommand h "\xf1" 6
-
-inCommand :: DeviceHandle -> BS.ByteString -> Size -> IO BS.ByteString
-inCommand h c s = do
-  -- send the command and make sure it was sucessful
-  (size, sendstatus) <- writeBulk h (EndpointAddress 1 Out) c 1000
-  guard $ size == BS.length c && sendstatus == Completed
-
-  -- receive the response, and ensure it's what we expected
-  (d, recstatus) <- readBulk h (EndpointAddress 1 In) s 1000
-  guard $ s == BS.length d && recstatus == Completed
-  pure d
-
-outCommand :: DeviceHandle -> BS.ByteString -> BS.ByteString -> IO BS.ByteString
-outCommand h c d = do
-  -- send the command and make sure it was sucessful
-  (sendsize, sendstatus) <- writeBulk h (EndpointAddress 1 Out) c 1000
-  guard $ sendsize == BS.length c && sendstatus == Completed
-
-  -- send the data
-  (recsize, recstatus) <- writeBulk h (EndpointAddress 1 Out) d 1000
-  guard $ recsize == BS.length d && recstatus == Completed
-  pure d
-
-openBoard :: IO ()
-openBoard = pickBoard >>=
-  maybe (pure ()) claimBoard
-
-claimBoard :: Device -> IO ()
-claimBoard d = withDeviceHandle d $ \h -> do
-  withDetachedKernelDriver h 0 $ do
-    withClaimedInterface h 0 $ do
-      getVersion h >>= print
+-- claimBoard :: Device -> IO ()
+-- claimBoard d = withDeviceHandle d $ \h -> do
+--   withDetachedKernelDriver h 0 $ do
+--     withClaimedInterface h 0 $ do
+--       getVersion h >>= print
